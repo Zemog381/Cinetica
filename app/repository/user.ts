3aeeb9882494fbd75852df0c014586f742c4e008
Zemog381 <remizemog07259@gmail.com>
2024-11-07 18:35:19 +0100

    change the password

--- app/repository/user.ts
+++ app/repository/user.ts
@@ -1,4 +1,4 @@
 export const user = {
-    username : 'cc',
-    passeword : '$2y$10$LuWKW/9ErwmdBHfzg/tHTO2//.D5.xr8R/nu9kg7D3PB1e2n5pjLy'
+    username : 'T1_Keria_<3',
+    passeword : '$2y$10$GvYKopL3vXw9IuzWGBCKAOkdV4Dn.AuFyWhVcD/QkankwihMmyW3e'
 };
