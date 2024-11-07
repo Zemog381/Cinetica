db7058571ca1c899e35ed04f42d8b15c22e5f2b2
Zemog381 <remizemog07259@gmail.com>
2024-11-07 18:18:51 +0100

    ajout page login

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,49 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import logoB from "./public/img/logo_b.png"
+import logoW from "./public/img/asuna.png"
+import { user } from "./repository/user"
+import bcrypt from 'bcryptjs';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isLogged,setIsLogged] = useState(false);
+  const [badLogin,setBadLogin] = useState(false);
+  const [userField,setUserField] = useState('');
+  const [passField,setPassField] = useState('');
+  const router = useRouter();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function handleClick() {
+    const pwdok = bcrypt.compareSync(passField,user.passeword)
+    if (user.username === userField && pwdok) setIsLogged(true)
+    else setBadLogin(true)
+  }
+
+
+  if (!isLogged)
+    return (
+    <>
+      <div className="grid h-screen place-items-center bg-black">
+        <div className="grid p-14 w-full max-w-4xl gap-2 bg-zinc-200/90 rounded-3xl">
+          <Image src={logoB} alt="le_logo"/>
+          <Label htmlFor="usr">Username</Label>
+          <Input id="usr" className="border-none bg-white max-h-5 resize-none" placeholder="Type your username here." onChange={e => setUserField(e.target.value)}/>
+          <Label htmlFor="pwd">Password</Label>
+          <Input id="pwd" className="border-none bg-white max-h-5 resize-none" placeholder="Type your password here." onChange={e => setPassField(e.target.value)}/>
+          <Button onClick={handleClick} >Login</Button>
+          {badLogin && (<p className= "text-red-600"> Wrong password and/or login, retry please</p>)}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        
+      </div>
+
+    </>
+  );  
+  else
+    router.push('./home')
 }
+
---
+++ app/repository/user.ts
@@ -0,0 +1,4 @@
+export const user = {
+    username : 'cc',
+    passeword : '$2y$10$LuWKW/9ErwmdBHfzg/tHTO2//.D5.xr8R/nu9kg7D3PB1e2n5pjLy'
+};
