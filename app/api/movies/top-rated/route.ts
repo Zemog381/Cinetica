48d06fd983d284e8b9a4c64f3245ec8fbb9c5b77
Zemog381 <remizemog07259@gmail.com>
2024-11-07 10:00:31 +0100

    fin tp1 ajout api

---
+++ app/api/movies/top-rated/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    const dotenv = require('dotenv').config();
+    const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer ' + process.env.KEY_API
+        }
+    };
+    const response = await fetch (url,options);
+    const data = await response.json();
+    return NextResponse.json(data);
+}
---
+++ app/api/shows/on-the-air/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    const dotenv = require('dotenv').config();
+    const url = 'https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer ' + process.env.KEY_API
+        }
+    };
+    const response = await fetch (url,options);
+    const data = await response.json();
+    return NextResponse.json(data);
+}
