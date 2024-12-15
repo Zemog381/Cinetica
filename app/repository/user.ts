8c9b96f640f3915c31cbbe27f0bb38a204eda118
Zemog381 <remizemog07259@gmail.com>
2024-12-15 19:14:03 +0100

    test en remettant nextauth_secret dans l'env

--- app/repository/user.ts
+++ app/repository/user.ts
@@ -2,4 +2,4 @@ export const user = {
     username : 'T1_Keria_<3',
     password : '$2y$10$GvYKopL3vXw9IuzWGBCKAOkdV4Dn.AuFyWhVcD/QkankwihMmyW3e'
 };
-export const NEXTAUTH_SECRET="fakerontop";
+//export const NEXTAUTH_SECRET="fakerontop";
