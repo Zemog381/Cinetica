ce5555566c50d6810a8811161ab95ee20bfa8232
runalian <bombay.marc@gmail.com>
2024-12-15 23:50:48 +0100

    page [movieID]

--- app/movies/[movieID]/page.tsx
+++ app/movies/[movieID]/page.tsx
@@ -1,6 +1,6 @@
 import { Movie } from '@/app/entities/Movie'
 import { headers } from 'next/headers'
-import { notFound } from 'next/navigation';
+import { notFound } from 'next/navigation'
 
 export default async function Page({
     params,
@@ -14,11 +14,25 @@ export default async function Page({
     if (!movieDetails.ok) return notFound(); // 404
     
     const mdm = await movieDetails.json() as Movie
+    const backdropUrl = "https://image.tmdb.org/t/p/w1280" + mdm.backdrop_path;
+    const posterUrl = "https://image.tmdb.org/t/p/w185" + mdm.poster_path;
+
     return (
-    <> 
-    <div>ID : {movieID}</div>
-    <p>{Number(movieID)}</p>
-    <p>{JSON.stringify(mdm)}</p>
-    </>
+      <div className="flex-col w-full">
+        <div className="flex w-full bg-no-repeat bg-cover" style={{backgroundImage:"url("+backdropUrl+")"}}>
+          <div className="m-2 h-[278px] w-full flex flex-row bg-white bg-opacity-30">
+            <div className="w-[185px] h-[278px] shrink-0 bg-no-repeat bg-cover" style={{backgroundImage:"url("+posterUrl+")"}}></div>
+             <div className="ml-2 flex-col">
+              <p className="text-4xl">{mdm.title}</p>
+              <p className="text-xl"><i>{mdm.tagline}</i></p>
+              <p className="text-xl">{mdm.release_date}</p>
+              <p className="text-lg">{mdm.overview}</p>
+             </div>
+          </div>
+        </div>
+        <div>
+          a
+        </div>
+      </div>
     );
   }
